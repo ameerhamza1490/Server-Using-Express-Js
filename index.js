5777537366211a276238b23dc01c54d541ec2658
ameerhamza1490 <ameerhamza1490@gmail.com>
2025-03-17 10:07:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//sb se pahlay express js ko install krain gay cmd k through phir import krain gay. us k bad app name k variable main usay store krdain. server create ho jaye ga aise
 import express from "express";
 const app = express();
 
+//app.get ka mtlb hai k mujhai user ki taraf se jo bi request aaye. or res.send k through main us user ko response send krunga
 app.get("/", (req, res) => {
   console.log(`Hello World ${new Date()} ${req.url}.`);
   res.send(`Hello World ${new Date()}.`);
@@ -11,6 +13,7 @@ app.get("/home", (req, res) => {
   res.send(`Home ${Date.now()}.`);
 });
 
+//PORT capital main likhay gay hamesha. process.env.PORT is ka mtlab hai k mujhai jo bhi url assign ho mera server kam kray us par.
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
